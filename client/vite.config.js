d04f0e79bf2e934f6f797b741c023cea34921f10
Vijay Kant <vijaykm9354@gmail.com>
2026-06-06 14:26:27 +0530

    fix: deployment workflow

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,11 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  plugins: [react()]
+export default defineConfig(({ command }) => {
+  return {
+    plugins: [react()],
+    // If we are building for production, use the GitHub Pages path.
+    // If we are running locally (dev), use the root '/' path.
+    base: command === 'build' ? '/personal-task-manager/' : '/',
+  };
 });
